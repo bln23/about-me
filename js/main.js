aaa87ce09e1217f7ffa4459140a2c44ad01433b2
Belén <be23mg@gmail.com>
2018-03-19 19:31:35 +0100

    button scroll slowly

--- js/main.js
+++ js/main.js
@@ -1,1 +1,10 @@
 'use strict';
+
+var button = document.querySelector('.button-arrow.down');
+button.addEventListener('click', showDown);
+
+function showDown(event) {
+  event.preventDefault();
+  var buttonDown = document.getElementById('next-section');
+  buttonDown.scrollIntoView({block: "start", behavior: "smooth"});
+}
