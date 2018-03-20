dd2f58157ea997206da3861e9c2d411a62759941
Belén <be23mg@gmail.com>
2018-03-20 12:17:10 +0100

    add button menu

--- js/main.js
+++ js/main.js
@@ -1,10 +1,29 @@
 'use strict';
 
-var button = document.querySelector('.button-arrow.down');
-button.addEventListener('click', showDown);
+// var button = document.querySelector('.button-arrow.down');
+// button.addEventListener('click', showDown);
+//
+// function showDown(event) {
+//   event.preventDefault();
+//   var buttonDown = document.getElementById('next-section');
+//   buttonDown.scrollIntoView({block: "start", behavior: "smooth"});
+// }
 
-function showDown(event) {
-  event.preventDefault();
-  var buttonDown = document.getElementById('next-section');
-  buttonDown.scrollIntoView({block: "start", behavior: "smooth"});
-}
+// Variables
+var hero = document.querySelector('.hero');
+var menuOpen = hero.querySelector('.menu-open');
+var menuClose = hero.querySelector('.menu-close');
+
+// Functions
+function openNavMenu() {
+	hero.classList.add('menu-visible');
+};
+
+function closeNavMenu() {
+	hero.classList.remove('menu-visible');
+};
+
+
+// Magic
+menuOpen.addEventListener('click', openNavMenu);
+menuClose.addEventListener('click', closeNavMenu);
